11dce3b7ec6ba49f370cda37af25940d4f444a53
Naifar_Sirine <sirine.naifer@esprit.tn>
2018-02-07 11:53:51 +0100

    test

---
+++ src/IServices/IAdmin.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IServices;
+
+import edu.entites.Admin;
+
+/**
+ *
+ * @author LENOVO
+ */
+public interface IAdmin{
+     public void modifierAdmin (Admin a2 );
+}
